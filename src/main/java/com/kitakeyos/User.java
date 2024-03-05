eb53a29e626c3e3975b043d21b0653a55c616085
Hoàng Hữu Dũng <kitakeyos@gmail.com>
2024-03-05 20:13:15 +0700

    refactor: remove unused import libraries

--- src/main/java/com/kitakeyos/User.java
+++ src/main/java/com/kitakeyos/User.java
@@ -5,18 +5,12 @@ import com.kitakeyos.interfaces.IReceiver;
 import com.kitakeyos.interfaces.ISender;
 import com.kitakeyos.util.Log;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-
 public class User implements ISender, IReceiver {
     private String name;
-    private int key;
     private SubstitutionCipher cipher;
 
     public User(String name, int key) {
         this.name = name;
-        this.key = key;
         char[] substitution = new char[]{
                 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
                 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
